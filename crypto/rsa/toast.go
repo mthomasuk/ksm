ae7bab599472fb3970efc831639712595dee34f7
Mark Thomas <mark@fiit.tv>
2019-05-13 09:27:43 +0100

    Better error handling

--- crypto/rsa/toast.go
+++ crypto/rsa/toast.go
@@ -1,6 +1,8 @@
 package rsa
 
 import (
+	"log"
+
 	"github.com/mthomasuk/toast/crypto"
 	"github.com/mthomasuk/toast/rsa"
 )
@@ -13,6 +15,7 @@ func OAEPPDecrypt(pub, pri string, ciphertext []byte) ([]byte, error) {
 		rsa.ParsePKCS1KeyByCert)
 
 	if err != nil {
+		log.Printf("Loading key from PEM byte(s) failed :: %v", err)
 		panic(err)
 	}
 
@@ -22,11 +25,13 @@ func OAEPPDecrypt(pub, pri string, ciphertext []byte) ([]byte, error) {
 
 	cipher, err := crypto.NewRSAWith(key, padding, cipherMode, signMode)
 	if err != nil {
+		log.Printf("Creating cipher from RSA key failed :: %v", err)
 		panic(err)
 	}
 
 	deT, err := cipher.Decrypt(ciphertext)
 	if err != nil {
+		log.Printf("Decrypting ciphertext failed :: %v", err)
 		panic(err)
 	}
 	return deT, err
